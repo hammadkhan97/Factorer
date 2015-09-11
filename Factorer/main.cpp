899a75af4dd1119267f272a6258997ee636e6f55
hakhan2 <hakhan2@ug-mac26.ics.illinois.edu>
2015-09-11 17:52:45 -0500

    Initial Commit

---
+++ Factorer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Factorer
+//
+//  Created by hakhan2 on 9/11/15.
+//  Copyright (c) 2015 hakhan2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
